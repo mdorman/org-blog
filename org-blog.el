29216e7d112b1bd787ec98519b30f8f039986ab1
Michael Alan Dorman <mdorman@ironicdesign.com>
2013-04-12 07:48:11 -0400

    Add org-blog-new.
    Complete with a series of tests for it.  Now we only lack posting for
    basic features.

--- org-blog.el
+++ org-blog.el
@@ -43,6 +43,35 @@ Commands:
 There are a number of parameters.  Some day I will enumerate
 them.")
 
+(defun org-blog-new ()
+  "Create a new buffer primed for a blog entry.
+
+If the user has only one blog defined in the `org-blog-alist',
+that blog will be used by default, otherwise the user will be
+prompted to chose the blog for which the post is intended.  The
+user can choose to enter a name that is not configured if they
+desire, and when they go to save the buffer, they will be
+prompted for information on how to post to the blog.
+
+The new buffer is initialized with the name of the blog, a
+timestamp reflecting the current time, and a number of other
+empty fields that the user may wish to fill in."
+  (interactive)
+  (let ((name (org-blog-get-name)))
+    (switch-to-buffer (generate-new-buffer (format "*org-blog/%s*" name)))
+    (org-mode)
+    (org-blog-mode)
+    ;; FIXME: Get defaults from org-blog-alist entry for the blog
+    (org-blog-buffer-merge-post `((:blog . ,name)
+                                  (:category . "")
+                                  (:date ,(current-time))
+                                  (:excerpt . "")
+                                  (:format . "post")
+                                  (:status . "publish")
+                                  (:tags . "")
+                                  (:title . "")
+                                  (:type . "post")))))
+
 (defun org-blog-get-name (&optional post)
   "Get a name of a blog, perhaps associated with a post.
 
@@ -89,4 +118,57 @@ we default to unknown."
     "Test getting the blog name from default"
     (with-mock
      (stub completing-read => "")
-     (should (string= (org-blog-get-name) "unknown")))))
+     (should (string= (org-blog-get-name) "unknown"))))
+  (ert-deftest ob-test-org-blog-new-from-alist ()
+    "Test creating a new blog post with an alist"
+    (with-mock
+     (stub current-time => '(20738 4432))
+     (let ((org-blog-alist '(("bar")))
+           (post-string "\
+#+POST_BLOG: bar
+#+POST_CATEGORY: 
+#+DATE: [2013-01-25 Fri 00:00]
+#+DESCRIPTION: 
+#+POST_STATUS: publish
+#+KEYWORDS: 
+#+TITLE: 
+#+POST_TYPE: post
+"))
+       (org-blog-new)
+       (should (string= (org-no-properties (buffer-string)) post-string)))))
+  (ert-deftest ob-test-org-blog-new-from-completing-read ()
+    "Test creating a new blog post using completing-read"
+    (with-mock
+     (stub current-time => '(20738 4432))
+     (stub completing-read => "baz")
+     (let ((post-string "\
+#+POST_BLOG: baz
+#+POST_CATEGORY: 
+#+DATE: [2013-01-25 Fri 00:00]
+#+DESCRIPTION: 
+#+POST_STATUS: publish
+#+KEYWORDS: 
+#+TITLE: 
+#+POST_TYPE: post
+"))
+       (org-blog-new)
+       (should (string= (org-no-properties (buffer-string)) post-string)))))
+  (ert-deftest ob-test-org-blog-new-from-default ()
+    "Test creating a new blog post with a default"
+    (with-mock
+     (stub current-time => '(20738 4432))
+     (stub completing-read => "")
+     (let ((org-blog-alist '(("bar")))
+           (post-string "\
+#+POST_BLOG: bar
+#+POST_CATEGORY: 
+#+DATE: [2013-01-25 Fri 00:00]
+#+DESCRIPTION: 
+#+POST_STATUS: publish
+#+KEYWORDS: 
+#+TITLE: 
+#+POST_TYPE: post
+"))
+       (org-blog-new)
+       (should (string= (org-no-properties (buffer-string)) post-string)))))
+)
